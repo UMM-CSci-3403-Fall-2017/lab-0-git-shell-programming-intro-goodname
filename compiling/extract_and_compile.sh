1a3eb38dc3f89bb908451cc31fd8ec9e5d5d0e19
holma198 <holma198@morris.umn.edu>
2017-08-29 21:29:25 -0500

    Add descriptions to our bash script Remove echo tests

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/bash
-echo "hi"
+
+
+##First argument passed in, used in the NthPrime program
 firstNum=$1
+
+##Second argument passed in, used to save the uncompressed and extracted
+##gunzip file
 dirName=$2
 
+##Extracts NthPrime.gtz to the give directory
 tar -xf NthPrime.tgz --$dirName
 
-echo "hi"
-
+##Compiles the NthPrime file to NthPrime directory and makes it executable
 gcc ./NthPrime/*.c -o ./NthPrime/NthPrime
 
+##Runs the NthPrime program with the given argument
 ./NthPrime/NthPrime $firstNum
 
 

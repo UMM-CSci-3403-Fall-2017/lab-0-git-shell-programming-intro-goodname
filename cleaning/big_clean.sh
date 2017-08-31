befa6005c39dac31cb0464d73519bacc0c306810
hoff0899 <hoff0899@morris.umn.edu>
2017-08-31 17:31:18 -0500

    Add remaining comments

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -6,13 +6,16 @@ tarchive=$1
 ##Name of Scratch directory
 scratchDir=$2
 
+##Saving starting directory
 here=$(pwd)
 
+##copying the tarchive file without the .tgz and saving it as a variable
 tarName=$(echo $tarchive | cut -f 1 -d '.')
 
 ##Extracts contents from big_dir.tgz into scratchDir
 tar -xf $tarchive --directory $scratchDir
 
+##Moved into scratch directory
 cd $scratchDir
 
 ##Finds all files in the scratchDir directory containing a "DELETE ME!" line
@@ -22,4 +25,5 @@ rm `grep -lr "DELETE ME!" $tarName`
 ##last argument tarchive without tgz
 tar -zcf "cleaned_$tarchive" $tarName
 
+##Move cleaned archive from scratch directory into cleaning
 mv "cleaned_$tarchive" $here

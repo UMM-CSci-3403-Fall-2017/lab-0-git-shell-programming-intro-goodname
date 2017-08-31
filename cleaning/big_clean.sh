d737dde421aa72903fd929cc267930ada8e3edc3
hoff0899 <hoff0899@morris.umn.edu>
2017-08-31 10:58:44 -0500

    Remove directory creation Create archive instead Copied content of `scratchDir` to `clean_tarchive`

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -1,25 +1,23 @@
 #!/usr/bin/bash
 
 ##Name of compressed tgz file
-$1=tarchive
+tarchive=$1
 
 ##Name of Scratch directory
-$2=scratchDir
+scratchDir=$2
 
 ##Extracts contents from big_dir.tgz into scratchDir
 tar -xf big_dir.tgz --$scratchDir
+
+tar -czf cleaned_$tarchive $(pwd)
 
 ##Finds all files in the scratchDir directory containing a "DELETE ME!" line
 ##and deletes that file
 rm `grep -lr "DELETE ME!" $scratchDir`
 
 ##Create directory for cleaned tar files
-`mkdir ./$scratchDir/cleaned_$tarchive`=cleanDir
+##`mkdir ./$scratchDir/cleaned_$tarchive`=cleanDir
 
-##cp -R $scratchDir/. $cleanDir
-
-##tar -z $cleanDir
-
-##./$scratchDir/
+cp -R $scratchDir/. $cleanDir
 
 

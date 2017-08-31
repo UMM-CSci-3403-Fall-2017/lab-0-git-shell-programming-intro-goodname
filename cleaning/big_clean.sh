7441dcf72152c9f55b1cd17a1eabe5f111018c77
hoff0899 <hoff0899@morris.umn.edu>
2017-08-31 14:57:10 -0500

    Add a $here function to capture current directory. Made a change where the gzip argument is added to the directory name rather than just bigdir.tgz.

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -6,10 +6,12 @@ tarchive=$1
 ##Name of Scratch directory
 scratchDir=$2
 
-##Extracts contents from big_dir.tgz into scratchDir
-tar -xf big_dir.tgz --$scratchDir
+here=$(pwd)
 
-tar -czf cleaned_$tarchive $(pwd)
+##Extracts contents from big_dir.tgz into scratchDir
+tar -xf $tarchive --$scratchDir
+
+tar -czf cleaned_$tarchive $here
 
 ##Finds all files in the scratchDir directory containing a "DELETE ME!" line
 ##and deletes that file
@@ -18,6 +20,6 @@ rm `grep -lr "DELETE ME!" $scratchDir`
 ##Create directory for cleaned tar files
 ##`mkdir ./$scratchDir/cleaned_$tarchive`=cleanDir
 
-cp -R $scratchDir/. $cleanDir
+cp -R $scratchDir/. ./cleaned_$tarchive
 
 
